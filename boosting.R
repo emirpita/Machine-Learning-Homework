2649e527ab4960a522f60bf1697c91a7ed07eb53
SamraMujcinovic <smujcinovi1@etf.unsa.ba>
2020-11-28 19:25:55 +0100

    Druga i treca iteracija boosting metode

--- boosting.R
+++ boosting.R
@@ -10,6 +10,10 @@ library(ISLR)
 library(tree)
 library(ipred)
 
+library(randomForest)
+library(gbm)
+library(ROSE)
+
 # Ucitavanje podataka (izvornih) u data frame
 atrain<-read.csv("Data/attrition_train.csv", header=TRUE)
 atest<-read.csv("Data/attrition_test.csv", header=TRUE)
@@ -49,12 +53,36 @@ confusionMatrix(as.factor(boost.pred),as.factor(atest$Attrition), positive = "1"
 #Druga iteracija#
 atrain$Attrition<-ifelse(atrain$Attrition=="Yes",1,0)
 atest$Attrition<-ifelse(atest$Attrition=="Yes",1,0)
-atrain$Attrition <- as.character(atrain$Attrition)
 
+atrain[,c(2:3,5,7:8,11:12,14:18,22:23,25:26,28,31,36)]<-lapply(atrain[,c(2:3,5,7:8,11:12,14:18,22:23,25:26,28,31,36)],as.factor)
+atrain$Attrition <- as.character(atrain$Attrition)
 boost.atrain<-gbm(Attrition~.-EmployeeCount-Over18-StandardHours, data=atrain, distribution="bernoulli", n.trees=10000, interaction.depth=8)
 boost.pred<-predict.gbm(boost.atrain, atest, type = "response", n.trees=10000)
 
 boost.pred<-ifelse(boost.pred>mean(boost.pred),1,0)
 confusionMatrix(as.factor(boost.pred),as.factor(atest$Attrition), positive = "1")
 
+#TRECA ITERACIJA
+#oversampling
+data_over<-ovun.sample(Attrition~., data = atrain, 
+                       method="over", N=1960)$data
+atrain[,c(2:3,5,7:8,11:12,14:18,22:23,25:26,28,31,36)]<-lapply(atrain[,c(2:3,5,7:8,11:12,14:18,22:23,25:26,28,31,36)],as.factor)
+atrain$Attrition <- as.character(atrain$Attrition)
+over.fit <- gbm(Attrition~.-EmployeeCount-Over18-StandardHours, data=atrain, distribution="bernoulli", n.trees=10000, interaction.depth=8)
+over.pred <- predict.gbm(over.fit, atest, type = "response", n.trees=10000)
+over.pred<-ifelse(over.pred>mean(over.pred),1,0)
+confusionMatrix(as.factor(over.pred),as.factor(atest$Attrition), positive = "1")
+
+
+#CETVRTA ITERACIJA
+#undersampling
+data_under<-ovun.sample(Attrition~., data = atrain, 
+                        method="under", N=600, seed = 1)$data
+
+atrain[,c(2:3,5,7:8,11:12,14:18,22:23,25:26,28,31,36)]<-lapply(atrain[,c(2:3,5,7:8,11:12,14:18,22:23,25:26,28,31,36)],as.factor)
+atrain$Attrition <- as.character(atrain$Attrition)
+under.fit <- gbm(Attrition~.-EmployeeCount-Over18-StandardHours, data=atrain, distribution="bernoulli", n.trees=10000, interaction.depth=8)
+under.pred <- predict.gbm(under.fit, atest, type = "response", n.trees=10000)
+under.pred<-ifelse(over.pred>mean(over.pred),1,0)
+confusionMatrix(as.factor(under.pred),as.factor(atest$Attrition), positive = "1")
 
